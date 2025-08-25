1625ded993e8db69921a3ede237559b459442cd1
SoRa <sonamraisx007@gmail.com>
2025-08-25 08:36:32 +0545

    Week: 2, Day: 3

---
+++ mobile/src/app/_layout.tsx
@@ -0,0 +1,31 @@
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Slot, Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import "react-native-reanimated";
+import "./globals.css";
+
+import { useColorScheme } from "@/src/hooks/useColorScheme";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+  });
+
+  if (!loaded) {
+    // Async font loading only occurs in development.
+    return null;
+  }
+
+  return (
+    <SafeAreaView className="bg-red-500">
+      <Slot />
+    </SafeAreaView>
+  );
+}
